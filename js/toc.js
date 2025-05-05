ac43365ac756336393f6cfa6820455337d6271f2
Tyler Feemster <ttfeemster@gmail.com>
2025-05-05 00:02:36 -0400

    making toc more aesthetic. added some js for toc -> post loading

--- js/toc.js
+++ js/toc.js
@@ -1,8 +1,15 @@
 const body = document.querySelector(".body-template");
-const postContent = document.getElementById('post-content');
+const postContent = document.getElementById("post-content");
 
 // Functions
 async function loadHtmlFile(filename) {
+  // remove title
+  const title = document.getElementById("writing-title");
+  title.remove();
+  //remove contents
+  const contents = document.getElementById("toc");
+  contents.remove();
+  // load html file
   const filepath = "../posts/" + filename + ".html";
   try {
     const response = await fetch(filepath);
@@ -11,7 +18,7 @@ async function loadHtmlFile(filename) {
       const newDiv = document.createElement("div");
       newDiv.className = "body-text";
       newDiv.innerHTML = htmlContent;
-      postContent.innerHTML = '';
+      postContent.innerHTML = "";
       postContent.appendChild(newDiv);
       renderMathInElement(postContent, {
         delimiters: [
@@ -30,30 +37,31 @@ async function loadHtmlFile(filename) {
 }
 
 async function fetchPosts() {
-    try {
-      const response = await fetch("../contents.json");
-      const posts = await response.json();
-      return posts;
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      return [];
-    }
+  try {
+    const response = await fetch("../contents.json");
+    const posts = await response.json();
+    return posts;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
+  }
 }
 
 async function generateTOC() {
-    const toc = document.getElementById('toc');
-    console.log(toc);
-    await fetchPosts().then((posts) => {
-        console.log(posts);
-        posts.forEach((post) => {
-          const listItem = document.createElement("li");
-          const link = document.createElement("a");
-          link.text = post.title;
-          link.onclick = async () => loadHtmlFile(post.file);
-          listItem.appendChild(link);
-          toc.appendChild(listItem);
-        });
+  const toc = document.getElementById("toc");
+  console.log(toc);
+  await fetchPosts().then((posts) => {
+    console.log(posts);
+    posts.forEach((post) => {
+      const item = document.createElement("h2");
+      item.className = "toc-item";
+      const link = document.createElement("a");
+      link.text = post.title;
+      link.onclick = async () => loadHtmlFile(post.file);
+      item.appendChild(link);
+      toc.appendChild(item);
     });
+  });
 }
 
 generateTOC();
